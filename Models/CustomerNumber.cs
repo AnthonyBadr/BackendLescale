6ab19dd9234cf4e0c26606e09fe40d4a3667a22d
AnthonyBadr <anthonybadr40@gmail.com>
2024-10-16 20:40:19 +0300

    Done

--- Models/CustomerNumber.cs
+++ Models/CustomerNumber.cs
@@ -2,7 +2,7 @@
 {
     public class CustomerNumber
     {
-        public string Name { get; set; }
+        public string CustomerName { get; set; }
         public string PhoneNumber { get; set; }
         public string Location { get; set; }
         public string Description { get; set; }
